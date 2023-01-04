da3caf1da0aba2575a743e162d452e0a8a8dc88a
shammalie <louismb@msn.com>
2023-01-04 22:22:33 +0000

    Add port to env variable list, extract hostname from os.

--- cmd/main.go
+++ cmd/main.go
@@ -4,16 +4,23 @@ import (
 	"fmt"
 
 	"github.com/shammalie/go-network-monitor/pkg"
-)
-
-const (
-	hostname = "localhost"
-	port     = 4320
+	"github.com/spf13/viper"
 )
 
 func main() {
-	server := pkg.NewGrpcServer(port, hostname)
-	fmt.Printf("starting server %s:%d\n", hostname, port)
+	viper.AddConfigPath(".")
+	viper.SetConfigName("app")
+	viper.SetConfigType("env")
+
+	viper.AutomaticEnv()
+
+	err := viper.ReadInConfig()
+	if err != nil {
+		panic(err)
+	}
+
+	server := pkg.NewGrpcServer()
+	fmt.Printf("starting server %s:%d\n", server.Hostname, server.Port)
 	go func() {
 		for e := range server.NetworkCaptureServer.ClientEvents {
 			fmt.Println(e)
--- pkg/grpcServer.go
+++ pkg/grpcServer.go
@@ -3,9 +3,11 @@ package pkg
 import (
 	"fmt"
 	"net"
+	"os"
 
 	heartbeat_v1 "github.com/shammalie/go-network-monitor/pkg/heartbeat.v1"
 	network_capture_v1 "github.com/shammalie/go-network-monitor/pkg/network_capture.v1"
+	"github.com/spf13/viper"
 	"google.golang.org/grpc"
 )
 
@@ -17,16 +19,24 @@ const (
 type grpcServer struct {
 	Heartbeat            *heartbeat_v1.HeartbeatServer
 	NetworkCaptureServer *network_capture_v1.NetworkCaptureServer
+	Hostname             string
+	Port                 int
 	listener             net.Listener
 	server               *grpc.Server
 }
 
 // Create a new grpc server that implements the handler for network_capture.v1#
 // Returns a grpc.Server pointer struct.
-func NewGrpcServer(port int, hostname string) *grpcServer {
-	if hostname == "" {
-		hostname = "localhost"
+func NewGrpcServer() *grpcServer {
+	hostname, err := os.Hostname()
+	if err != nil {
+		panic(err)
 	}
+	port := viper.GetInt("SERVER_PORT")
+	if port == 0 {
+		port = 3000
+	}
+
 	lis, err := net.Listen("tcp", fmt.Sprintf("%s:%d", hostname, port))
 	if err != nil {
 		panic(fmt.Sprintf(logErrorPrefix, err))
@@ -42,6 +52,8 @@ func NewGrpcServer(port int, hostname string) *grpcServer {
 	return &grpcServer{
 		Heartbeat:            heartbeatServer,
 		NetworkCaptureServer: networkCaptureServer,
+		Hostname:             hostname,
+		Port:                 port,
 		listener:             lis,
 		server:               s,
 	}
