a4460022428489aaceee2c706310ccc3b57b93b2
shammalie <louismb@msn.com>
2023-01-08 18:36:46 +0000

    Add ipv6 lookback to private check.

--- internal/ipUtils.go
+++ internal/ipUtils.go
@@ -3,19 +3,31 @@ package internal
 import (
 	"errors"
 	"net"
+	"strings"
 )
 
 func IsPrivateIP(ip string) (bool, error) {
 	var err error
 	private := false
-	IP := net.ParseIP(ip)
+	var IP net.IP
+	if strings.Contains(ip, "/") {
+		IP, _, err = net.ParseCIDR(ip)
+	} else {
+		IP = net.ParseIP(ip)
+	}
 	if IP == nil {
 		err = errors.New("invalid ip")
 	} else {
 		_, private24BitBlock, _ := net.ParseCIDR("10.0.0.0/8")
 		_, private20BitBlock, _ := net.ParseCIDR("172.16.0.0/12")
 		_, private16BitBlock, _ := net.ParseCIDR("192.168.0.0/16")
-		private = private24BitBlock.Contains(IP) || private20BitBlock.Contains(IP) || private16BitBlock.Contains(IP)
+		_, private8BitBlock, _ := net.ParseCIDR("127.0.0.0/8")
+		_, privateIpv6LoBlock, err := net.ParseCIDR("::1/128")
+		if err != nil {
+			panic(err)
+		}
+
+		private = private24BitBlock.Contains(IP) || private20BitBlock.Contains(IP) || private16BitBlock.Contains(IP) || private8BitBlock.Contains(IP) || privateIpv6LoBlock.Contains(IP)
 	}
 	return private, err
 }
